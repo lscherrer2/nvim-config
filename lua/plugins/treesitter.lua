adf639c3c99f5fbe012758589dfe9317d5c541e9
lscherrer2 <lukescherrer2@gmail.com>
2025-06-22 07:51:51 -0700

    Astro nvim default

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate"
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
 }
